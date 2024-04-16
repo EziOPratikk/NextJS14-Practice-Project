824c57b4a6d9da2210800693521b733f2a72b381
Pratik <khadkapratikk10@gmail.com>
2024-04-16 18:00:32 +0545

    add register page

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,3 +1,33 @@
+import { handleUserRegister } from '@/lib/action';
+import classes from './page.module.css';
+
 export default function Register() {
-  return <h1>Register Page</h1>;
+  return (
+    <div className={classes.registerContainer}>
+      <h1>Register</h1>
+      <form action={handleUserRegister} className={classes.form}>
+        <input type='text' placeholder='Username' name='username' required />
+        <input type='email' placeholder='Email' name='email' required />
+        <input
+          type='url'
+          placeholder='Avatar Image URL (optional)'
+          name='img-url'
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          name='password'
+          required
+        />
+        <input
+          type='password'
+          placeholder='Confirm Password'
+          name='confirm-password'
+          required
+        />
+
+        <button type='submit'>Register</button>
+      </form>
+    </div>
+  );
 }
