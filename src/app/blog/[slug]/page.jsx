66db66e3d4a6c596382396f635897301069d4406
Pratik <khadkapratikk10@gmail.com>
2024-04-19 12:15:01 +0545

    make minor changes

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -3,15 +3,15 @@ import Image from 'next/image';
 import classes from './page.module.css';
 import { handleGetSpecificPost, handleGetSpecificUser } from '@/lib/data';
 
-export async function generateMetadata({params}) {
+export async function generateMetadata({ params }) {
   const slug = params.slug;
-  
+
   const post = await handleGetSpecificPost(slug);
 
   return {
     title: post.title,
     description: post.description,
-  }
+  };
 }
 
 export default async function SinglePostBlog({ params }) {
@@ -19,14 +19,14 @@ export default async function SinglePostBlog({ params }) {
 
   const post = await handleGetSpecificPost(slug);
 
-  const user = await handleGetSpecificUser(post.userId);
+  const user = await handleGetSpecificUser(post.username);
 
   return (
     <div className={classes.singlePostContainer}>
       <div className={classes.leftContainer}>
         <div className={classes.imgContainer}>
           <Image
-            src={post.image}
+            src={post.image ? post.image : '/images/no-image.png'}
             alt={'a ' + post.title + ' image'}
             fill
             className={classes.img}
@@ -54,7 +54,7 @@ export default async function SinglePostBlog({ params }) {
                 </tr>
                 <tr>
                   <td>{user.username}</td>
-                  <td>{post.createdAt}</td>
+                  <td>{post.createdAt.toString().substring(0, 15)}</td>
                 </tr>
               </tbody>
             </table>
